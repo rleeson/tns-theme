ed8fd9d1488cf3627fc6d93c1477e0a715a5c1a2
rleeson <sysadmin@ryanleeson.com>
2015-07-03 02:46:52 -0400

    First pass at a slider, uses fixed widths on elements, want this to become more dynamic, maybe use translations for the next round

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ function tns_script_include() {
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css' );
 	wp_enqueue_style( 'tns-main', get_template_directory_uri() . '/css/main.css', array( 'bootstrap' ) );
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', array( 'jquery' ), '3.3.4', true );
-	wp_enqueue_script( 'tns-main-js', get_template_directory_uri() . '/js/main.js', array( 'bootstrap-js' ), '1.0.0', true );
+	wp_enqueue_script( 'tns-main-js', get_template_directory_uri() . '/js/main.js', array( 'bootstrap-js', 'underscore' ), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'tns_script_include' );
 
--- index.php
+++ index.php
@@ -12,7 +12,25 @@
 
 get_header();
 ?>
-	<div class="content"><h1>Content Area</h1></div>
+	<div class="content">
+		<h1>Content Area</h1>
+		<div id="slider-1" class="slider">
+			<button class="slide-left">Left</button>
+			<ul class="slide-outer">
+				<li class="slide"><a href="">Item 1</a></li>
+				<li class="slide"><a href="">Item 2</a></li>
+				<li class="slide"><a href="">Item 3</a></li>
+				<li class="slide"><a href="">Item 4</a></li>
+				<li class="slide"><a href="">Item 5</a></li>
+				<li class="slide"><a href="">Item 6</a></li>
+				<li class="slide"><a href="">Item 7</a></li>
+				<li class="slide"><a href="">Item 8</a></li>
+				<li class="slide"><a href="">Item 9</a></li>
+				<li class="slide"><a href="">Item 10</a></li>
+			</ul>
+			<button class="slide-right">Right</button>
+		</div>
+	</div>
 <?php 
 get_sidebar();
 get_footer();
