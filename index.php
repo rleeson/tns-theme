8f3b550116b49e0e16d1e8fbb373b28b9f26f431
rleeson <sysadmin@ryanleeson.com>
2015-07-05 14:23:50 -0400

    Rough out base structure to test TNS slider

--- index.php
+++ index.php
@@ -14,21 +14,28 @@ get_header();
 ?>
 	<div class="content">
 		<h1>Content Area</h1>
-		<div id="slider-1" class="slider">
-			<button class="slide-left">Left</button>
-			<ul class="slide-outer">
-				<li class="slide"><a href="">Item 1</a></li>
-				<li class="slide"><a href="">Item 2</a></li>
-				<li class="slide"><a href="">Item 3</a></li>
-				<li class="slide"><a href="">Item 4</a></li>
-				<li class="slide"><a href="">Item 5</a></li>
-				<li class="slide"><a href="">Item 6</a></li>
-				<li class="slide"><a href="">Item 7</a></li>
-				<li class="slide"><a href="">Item 8</a></li>
-				<li class="slide"><a href="">Item 9</a></li>
-				<li class="slide"><a href="">Item 10</a></li>
-			</ul>
-			<button class="slide-right">Right</button>
+		<!-- #slider-2.slider-infinite>button.slide-left+div.slider-body>article.slide*14>a{Item $}^^button.slide-right -->
+		<div id="slider-2" class="slider-infinite">
+			<div class="slide-mask">
+				<button class="slide-left"></button>
+				<div class="slider-body">
+					<div class="slide"><a href="">Item 1</a></div>
+					<div class="slide"><a href="">Item 2</a></div>
+					<div class="slide"><a href="">Item 3</a></div>
+					<div class="slide"><a href="">Item 4</a></div>
+					<div class="slide"><a href="">Item 5</a></div>
+					<div class="slide"><a href="">Item 6</a></div>
+					<div class="slide"><a href="">Item 7</a></div>
+					<div class="slide"><a href="">Item 8</a></div>
+					<div class="slide"><a href="">Item 9</a></div>
+					<div class="slide"><a href="">Item 10</a></div>
+					<div class="slide"><a href="">Item 11</a></div>
+					<div class="slide"><a href="">Item 12</a></div>
+					<div class="slide"><a href="">Item 13</a></div>
+					<div class="slide"><a href="">Item 14</a></div>
+				</div>
+				<button class="slide-right"></button>
+			</div>
 		</div>
 	</div>
 <?php 
--- src/js/core.js
+++ src/js/core.js
@@ -2,40 +2,6 @@
  * Core JS File
  */
 
-var console = console || { log: function( e ) { return; } };
-var tns = tns || {};
+var tns = tns || { slider_infinite: { init: function() { return; } } };
 
-tns.slider = function( $, _ ) {
-	var base_element	= '.slider',
-		body_width		= 1200,
-		index			= 0,
-		slide_width		= 200,
-		slides			= 10;
-	
-	function slider() {
-		var $slider = $( base_element );
-		var $slide_body = $slider.children( '.slide-outer' );
-		
-		$slider.children( '.slide-left' ).click( function ( e ) {
-			if ( index > 0 ) {
-				var next = $slide_body.css( 'left', '+=' + slide_width );
-				index = index - 1;
-				$slide_body.css( 'left', next );
-			}
-		} );
-		$slider.children( '.slide-right' ).click( function ( e ) {
-			if ( index < ( slides - ( body_width / slide_width ) ) ) {
-				var next = $slide_body.css( 'left', '-=' + slide_width );
-				index = index + 1;
-				$slide_body.css( 'left', next );
-			}
-		} );
-	}
-	return {
-		init: function() {
-			slider();
-		}
-	}
-}( jQuery, _ );
-
-tns.slider.init();
+tns.slider_infinite.init( '#slider-2' );
